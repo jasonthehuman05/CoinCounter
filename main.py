9a459e942011c18bb953610a1879b7e00803b36f
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-10-30 15:40:38 +0000

    inserted functions to isolate objects within a certain boundary in the image. boundary does need fine-tuning

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import cv2
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
 while True:
     frameCaptured, frame = cam.read() #Load image from camera
 
-    greyscale = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    greyscale = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) #Makes the image greyscale. This will hopefully help isolate the coins better, since it will be looking
+                                                        # for shiny on a less vibrant background
+    #highlight things in a certain boundary
+    lb = np.array([128])
+    ub = np.array([255])
+
+    rangedimage = cv2.inRange(greyscale, lb, ub)
     
     cv2.imshow("Window", frame) #Display image in window
     cv2.imshow("Greyscale", greyscale) #Display image in window
+    cv2.imshow("Ranged Image", rangedimage) #Display image in window
     if cv2.waitKey(10)==27:
         break
